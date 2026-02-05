6534b06fc7a107aab508f4f6fb4682232c43d30c
InhaKr <inkr3891@gmail.com>
2026-02-05 18:17:55 +0200

    Part 1

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,10 +1,32 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
+export const USER_ID = 3943;
 
 export const getTodos = () => {
   return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
 };
 
-// Add more methods here
+// Add more methods here/Каждый todo — это объект примерно такого вида:
+// {
+//   "id": 1,
+//   "userId": USER_ID,
+//   "title": "Buy milk",
+//   "completed": false
+// }
+
+export const createTodo = ({
+  title,
+  userId = USER_ID,
+  completed = false,
+}: Omit<Todo, 'id'>) => {
+  return client.post<Todo>(`/todos`, { userId, title, completed });
+};
+
+export const deleteTodo = (todoId: number) => {
+  return client.delete(`/todos/${todoId}`);
+};
+
+export const updateTodo = (id: number, data: Partial<Todo>) => {
+  return client.patch<Todo>(`/todos/${id}`, data);
+};

1d099721c91ea441ea63ca6ca92f4e4ce64f4ef3
InhaKr <inkr3891@gmail.com>
2026-02-05 19:54:26 +0200

    Part 1.1

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const BASE_URL = 'https://mate.academy/students-api';
 
-const handleResponse = (response: Response) => {
-  if (!response.ok) {
-    throw new Error(`${response.status} ${response.statusText}`);
-  }
+// const handleResponse = (response: Response) => {
+//   if (!response.ok) {
+//     throw new Error(`${response.status} ${response.statusText}`);
+//   }
 
-  return response.json();
-};
+//   return response.json();
+// };
 
 // returns a promise resolved after a given delay
 function wait(delay: number) {
